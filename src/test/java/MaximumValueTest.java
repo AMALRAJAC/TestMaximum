42a6c0a940556546bd1a127887dc0d8b72b58e65
unknown <acamalraj@gmail.com>
2021-04-28 20:32:44 +0530

    add:program for String maximum

--- src/test/java/MaximumValueTest.java
+++ src/test/java/MaximumValueTest.java
@@ -3,7 +3,8 @@ import org.junit.jupiter.api.Test;
 
 public class MaximumValueTest {
     Integer[] intArray={10,30,20};
-
+    Double [] doubleArray={10.25,35.50,35.75};
+    String [] stringArray={"pear","apple","orange"};
     @Test
     public void maximumValueWillReturnFirstPosition(){
        int firstPosition=MaximumValue.maximumValue(intArray);
@@ -18,5 +19,35 @@ public class MaximumValueTest {
     public void maximumValueWillReturnThirdPosition(){
         int ThirdPosition=MaximumValue.maximumValue(intArray);
         Assertions.assertEquals(20,ThirdPosition);
+    }
+    @Test
+    public void maximumValueWillReturnFirstDoublePosition(){
+        double firstPosition=MaximumValue.maximumValue(doubleArray);
+        Assertions.assertEquals(10.25,firstPosition);
+    }
+    @Test
+    public void maximumValueWillReturnSecondDoublePosition(){
+        double SecondPosition=MaximumValue.maximumValue(doubleArray);
+        Assertions.assertEquals(30.50,SecondPosition);
+    }
+    @Test
+    public void maximumValueWillReturnThirdDoublePosition() {
+        double ThirdPosition = MaximumValue.maximumValue(doubleArray);
+        Assertions.assertEquals(35.75,ThirdPosition);
+    }
+    @Test
+    public void maximumValueWillReturnFirstStringPosition(){
+        String firstPosition=MaximumValue.maximumValue(stringArray);
+        Assertions.assertEquals("pear",firstPosition);
+    }
+    @Test
+    public void maximumValueWillReturnSecondStringPosition(){
+        String SecondPosition=MaximumValue.maximumValue(stringArray);
+        Assertions.assertEquals("apple",SecondPosition);
+    }
+    @Test
+    public void maximumValueWillReturnThirdStringPosition(){
+        String ThirdPosition = MaximumValue.maximumValue(stringArray);
+        Assertions.assertEquals("orange",ThirdPosition);
     }
 }
