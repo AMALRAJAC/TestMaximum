c54cb2d9d3d336bef6c0ccde0e6781a127a4d5fc
unknown <acamalraj@gmail.com>
2021-04-29 20:35:00 +0530

    add:program to take more than 3 parameters

--- src/test/java/MaximumValueTest.java
+++ src/test/java/MaximumValueTest.java
@@ -2,50 +2,25 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MaximumValueTest {
-
+    Integer[] intArray={10,30,20,15};
+    Double [] doubleArray={10.25,35.50,35.75,1.1};
+    String [] stringArray={"pear","apple","orange","peach"};
     @Test
     public void maximumValueWillReturnFirstPosition(){
-        int firstPosition=MaximumValue.maximumValue(10,20,30);
-        Assertions.assertEquals(10,firstPosition);
+        int firstPosition=MaximumValue.maximumValue(intArray);
+        Assertions.assertEquals(30,firstPosition);
     }
-    @Test
-    public void maximumValueWillReturnSecondPosition(){
-        int SecondPosition=MaximumValue.maximumValue(10,20,30);
-        Assertions.assertEquals(30,SecondPosition);
-    }
-    @Test
-    public void maximumValueWillReturnThirdPosition(){
-        int ThirdPosition=MaximumValue.maximumValue(10,20,30);
-        Assertions.assertEquals(20,ThirdPosition);
-    }
+
     @Test
     public void maximumValueWillReturnFirstDoublePosition(){
-        double firstPosition=MaximumValue.maximumValue(10.25,35.50,35.75);
-        Assertions.assertEquals(10.25,firstPosition);
+        double firstPosition=MaximumValue.maximumValue(doubleArray);
+        Assertions.assertEquals(35.75,firstPosition);
     }
-    @Test
-    public void maximumValueWillReturnSecondDoublePosition(){
-        double SecondPosition=MaximumValue.maximumValue(10.25,35.50,35.75);
-        Assertions.assertEquals(30.50,SecondPosition);
-    }
-    @Test
-    public void maximumValueWillReturnThirdDoublePosition() {
-        double ThirdPosition = MaximumValue.maximumValue(10.25,35.50,35.75);
-        Assertions.assertEquals(35.75,ThirdPosition);
-    }
+
     @Test
     public void maximumValueWillReturnFirstStringPosition(){
-        String firstPosition=MaximumValue.maximumValue("pear","apple","orange");
+        String firstPosition=MaximumValue.maximumValue(stringArray);
         Assertions.assertEquals("pear",firstPosition);
     }
-    @Test
-    public void maximumValueWillReturnSecondStringPosition(){
-        String SecondPosition=MaximumValue.maximumValue("pear","apple","orange");
-        Assertions.assertEquals("apple",SecondPosition);
-    }
-    @Test
-    public void maximumValueWillReturnThirdStringPosition(){
-        String ThirdPosition = MaximumValue.maximumValue("pear","apple","orange");
-        Assertions.assertEquals("orange",ThirdPosition);
-    }
+
 }
